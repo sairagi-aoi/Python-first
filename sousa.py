39b7b52929f76916855585372ac7511c247bd5ca
sairagi-aoi <jatszz61@gmail.com>
2025-08-04 21:50:38 +0900

    アプリ進めた

--- sousa.py
+++ sousa.py
@@ -1,9 +1,13 @@
-import tkinter as tk  #tkinter をインポートしてtkとして使う
+import tkinter as tk
+
+def dispLabel():
+    lbl.configure(text="こんにちは")
+
 root = tk.Tk() #画面を造る
 root.geometry("200x100")
 
 lbl = tk.Label(text="LABEL")#ラベルを作る
-btn = tk.Button(text= "PUSH")#ボタンを作る
+btn = tk.Button(text= "PUSH",command = dispLabel)
 
 lbl.pack()#画面にラベルを配置する
 btn.pack()#画面にボタンを配置する
