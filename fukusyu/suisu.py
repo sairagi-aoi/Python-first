7749ebd398b01efb90ceef55a890ecb1a4747e1f
sairagi-aoi <jatszz61@gmail.com>
2025-08-19 21:52:53 +0900

    アンパック代入等

---
+++ fukusyu/suisu.py
@@ -0,0 +1,13 @@
+print("半径が3cmの円の直径は、")
+dia = 3 * 2
+print(dia)
+print("その円の円周の長さは、")
+print(dia * 3.14)
+
+print("飼い猫の名前と年齢は？")
+name, age ="よつば", 1
+print("来年飼い猫は何歳になる？")
+print(age + 1)
+print("飼い猫の名前と年齢を教えて下さい")
+print(name, age ,"歳")
+
