fc44d8031d7eafb7f66162edc46695cd832aa3f0
sairagi-aoi <jatszz61@gmail.com>
2025-08-26 22:16:10 +0900

    “fストリング”

---
+++ fukusyu/ume.py
@@ -0,0 +1,25 @@
+name = "松田ひかり"
+age = 30
+height = 159.2
+print("私の名前は{}で、年齢は{}で、身長は{}cmです".format(name,age,height))
+
+
+name = "松田ひかり"
+age = 30
+height = 159.2
+print(f"私の名前は{name}で、年齢は{age}で、身長は{height}cmです")
+
+
+name = "佐藤茉莉"
+age = 29
+height = 160.0
+chronicillness = "うつ病"
+print(f"私の名前は{name}です。 年齢は{age}で、身長は{height}cmです。平均より少し高いかな？ あと、{chronicillness}の持病がありますが、元気に生活しています。")
+
+
+
+
+
+
+
+
