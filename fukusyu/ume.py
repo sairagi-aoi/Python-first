61d454ffdf66eadb8b81a89b6b79749306101708
sairagi-aoi <jatszz61@gmail.com>
2025-08-28 22:18:09 +0900

    “テキスト練習問題”

--- fukusyu/ume.py
+++ fukusyu/ume.py
@@ -16,7 +16,14 @@ height = 160.0
 chronicillness = "うつ病"
 print(f"私の名前は{name}です。 年齢は{age}で、身長は{height}cmです。平均より少し高いかな？ あと、{chronicillness}の持病がありますが、元気に生活しています。")
 
-
+name = "佐藤樹希"
+age = 35
+height = 170.0
+chronicillness = "うつ病"
+print(f"私の名前は{name}です。")
+print(f"年齢は今年で{age}歳になります。")
+print(f"身長は{height}cmです。周りの友達よりも結構高いですよ")
+print(f"あと、私は{chronic_illness}の持病がありますが、上手く付き合いつつ生活しています。")
 
 
 
