d4803962b0ec51ea49d4495480ccd0337c9e6690
sairagi-aoi <jatszz61@gmail.com>
2025-08-29 22:56:10 +0900

    “リスト構造”

---
+++ fukusyu/conte.py
@@ -0,0 +1,20 @@
+members = ["工藤","松田","浅木"]
+print(members)
+
+study = ["プログラミング","英語","科学"]
+print(study)
+
+study = ["プログラミング","英語","科学"]
+print(study[0])
+
+study = ["プログラミング","英語","科学"]
+print(study[2])
+print(study[1])
+
+scores = [90,80,70]
+total = sum(scores)
+print(f"合計{total}点")
+
+
+
+
