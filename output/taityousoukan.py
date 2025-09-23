eb15e372cc2cd97ffad50200d30c312dbccb0527
sairagi-aoi <jatszz61@gmail.com>
2025-09-23 22:20:54 +0900

    体調相関続

--- output/taityousoukan.py
+++ output/taityousoukan.py
@@ -10,4 +10,20 @@ def log_entry():
     cat = input("飼い猫の名前教えてください: ").strip()
 
 while True:
+    OWNWER_condition = input("あなたの体調を教えてください >> ").strip()
     
+    if OWNWER_condition in ("最悪","悪い"):
+        conddition_type = input("その体調不良は精神面と身体面のどちらですか？それとも両方ですか？（精神面/身体面/両方) >>").strip()
+        conddition_since = input("その体調不良はいつ頃から発生していますか？ >> ").strip()
+        conddition_cause = input("その体調不良の原因として思い当たることはありますか？（無い場合は「無し」と入力して下さい) >> ").strip()
+        break
+    elif OWNWER_condition in ("普通","良い","絶好調"):
+        print("良いですね！ その調子で無理なく楽しんでお過ごし下さい。")
+    else:
+        print("提示されている選択肢の中からお選びください")
+        break
+
+    CAT_condhition = input("飼い猫の様子を教えて下さい >> ").strip()
+
+    if CAT_condhition in ("ぐったり" ,"元気ない"):
+        cat_conthition_type = input("")
