ef72bbc5237c9464b5af1d495a5fa7d68dcb2b00
sairagi-aoi <jatszz61@gmail.com>
2026-04-07 22:19:26 +0900

    LOG_PATH = Path(__file__).with_name(dept_chek_log.csv) の意味と役割について学んだ。

--- スッキリ解る/条件/busyobunki.py
+++ スッキリ解る/条件/busyobunki.py
@@ -1,9 +1,9 @@
 from difflib import get_close_matches #defflibライブラリからget_close_matchesモジュールをインポートする
-from pathlib import path #pathlibライブラリからpatchモジュールをインポートする
+from pathlib import Path #pathlibライブラリからpatchモジュールをインポートする
 from datetime import datetime #datemeモジュールからdatetimeクラスをインポートする
 import csv
 
-DEPT_MASTER = {
+DEPT_MASTER = { #部署一覧のマスタ部分
     "jimuijika" : "医療事務",
     "sougousoudan" : "総合相談センター",
     "kango1ns" : "一病棟看護",
@@ -13,6 +13,8 @@ DEPT_MASTER = {
 }
 
 VALID_DEPT_IDS = set(DEPT_MASTER.keys()) #部署マスタのキーをsetでVALID_DEPT_IDSに代入する
+
+LOG_PATH = Path(__file__).with_name("dept_chek_log.csv") #このPythonファイルのパスをPathクラスに変換しpathlibで扱いやすいpathオブジェクトに変化させてファイル名をdept_chek_log.csvに変化させる
 
 
 
